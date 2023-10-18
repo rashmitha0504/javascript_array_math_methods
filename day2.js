afa4027697a9a14692283ee096155b6b113587ad
Rashmitha Gudipudi <145190134+rashmitha0504@users.noreply.github.com>
2023-10-18 14:10:33 +0530

    prep

---
+++ day2.js
@@ -0,0 +1,61 @@
+/*
+The JavaScript Math object allows you to perform mathematical tasks on numbers
+There are 4 common methods to round a number to an integer
+
+input_1:
+const numbers=9.7;
+
+1.round
+Rounds a number x to the nearest integer. 
+It rounds to the nearest even number if the number is exactly
+halfway between two integers.
+
+console.log(Math.round(numbers))
+
+2.ceil
+Rounds a number x up to the nearest integer.
+
+console.log(Math.ceil(numbers))
+
+3.floor
+Rounds a number x down to the nearest integer.
+
+console.log(Math.floor(numbers))
+
+4.random
+Generates a random floating-point number between 0 and 1 
+
+console.log(Math.random())
+
+**input_2**
+const numbers_2=-5.5;
+
+5.abs 
+Returns the absolute (positive) value of a number x
+
+console.log(Math.abs(numbers_2))
+
+6.max()
+Returns the largest of the supplied numbers.
+
+console.log(Math.max(10,20,30))
+
+7.min()
+Returns the smallest of the supplied numbers.
+
+console.log(Math.max(10,20,30))
+
+8.pow()
+Returns x raised to the power of y
+
+console.log(Math.pow(2,3))
+
+9.sqrt
+Returns the square root of x
+
+console.log(Math.sqrt(4))
+
+*/
+
+
+
